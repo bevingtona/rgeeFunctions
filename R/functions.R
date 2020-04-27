be6c564f8d1285ed43b199184cb1b218ef7561b0
Alexandre Bevington <alexandre.bevington@gov.bc.ca>
2020-04-26 21:56:03 -0700

    testing

--- R/functions.R
+++ R/functions.R
@@ -2,12 +2,27 @@
 #'
 #' This function allows you to express your love of cats.
 #' @param img this is the image
-#' @keywords date 
+#' @keywords date
 #' @export
 #' @examples
 #' dates()
 
+library(rgee)
+
 dates <- function(img) {
+  doy <- ee$Date(img$get('system:time_start'))$getRelative('day', 'year')
+  img$set('doy', doy)}
+
+#' A Date Function
+#'
+#' This function allows you to express your love of cats.
+#' @param img this is the image
+#' @keywords date
+#' @export
+#' @examples
+#' dates()
+
+dates2 <- function(img) {
   doy <- ee$Date(img$get('system:time_start'))$getRelative('day', 'year')
   img$set('doy', doy)}
 
